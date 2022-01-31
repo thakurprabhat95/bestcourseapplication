ac2bdae78e4b648758a12e31dbc89bc771ff99a5
thakurprabhat95 <thakurprabhat95@gmail.com>
2022-01-31 19:47:23 +0530

    adding react app

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import {  Col, Container, Row } from 'reactstrap';
+import { toast } from 'react-toastify';
+import Home from './Component/Home';
+import Allcourses from './Component/Allcourses';
+import AddCourse from './Component/AddCourses';
+import Header from './Component/Header';
+import Menus from './Component/Menus';
+import { BrowserRouter as Router,Route, Routes} from "react-router-dom";
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+function App() {
+
+
+  const btnHandle=()=>{
+    // toast("this is my first react app!!!")
+    toast.success("done!!",{
+      position:'top-center'
+    })
+  }
+
+  return ( 
+   <div>
+     {/* <ToastContainer/>
+     <h1>Welcome to Course Application!!</h1>
+     <Button color="danger" outline onClick={btnHandle}>
+       
+       First React Button</Button> */}
+       {/* <Home/> */}
+       {/* <Course course={{title:"React js course",description:"This is best course on youtube"}}/> */}
+       {/* <Allcourses/>
+       <AddCourse/> */}
+  
+    
+  
+      <Router>
+       <Container>
+       <Header/>
+       <Row>
+         <Col md={4}>
+             <Menus/>
+         </Col>
+         
+         <Col md={8}>
+         
+         <Routes>
+         <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/addcourse" element={<AddCourse/>}/>
+          <Route exact path="/viewcourse" element={<Allcourses/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
+          </Routes>
+         
+        
+         </Col>
+       </Row>
+     
+     </Container>
+     </Router>
+  
+    
+     
+     
+   </div>
+  );
+}
+
+export default App;
---
+++ src/apicall/bootapi.js
@@ -0,0 +1,2 @@
+const base_url="http://bestcourseapp-env.eba-4s22jke2.us-east-2.elasticbeanstalk.com/";
+export default base_url;
